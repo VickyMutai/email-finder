711fcf9571e919611c59df785d843d18d9a01b8d
VickyMutai <vicky.mutai96@gmail.com>
2020-01-31 10:54:19 +0300

    new dependencies added

--- index.py
+++ index.py
@@ -6,31 +6,30 @@ from bs4 import BeautifulSoup
 
 #declaring variables
 allLinks = [];mails=[]
-
-url = "https://www.assentcompliance.com/"
+url = "https://www.assentcompliance.com/about-leadership-team/"
 response = requests.get(url)
 #parse text and get links
 soup = BeautifulSoup(response.text, 'html.parser')
 links = [a.attrs.get('href') for a in soup.select('a[href]')]
 
 for i in links:
-    if (("contact" in i or "Contact" in i) or ("Career" in i or "career" in i )) or ('about' in i or "About" in i) or ('Services' in i or 'services' in i):
+    if (("mailto" in i) or ("contact" in i or "Contact" in i) or ("Career" in i or "career" in i )) or ('about' in i or "About" in i) or ('Services' in i or 'services' in i):
         allLinks.append(i)
+    if '@' in i:
+        mails.append(i)
+print(mails)
 
 #function for finding mails
 def findMails(soup):
     for name in soup.find_all('a'):
-        print(name)
         if (name is not None):
             emailText = name.text
             #checks if text is an email using regex expressions and returns a boolean
-            match = bool(re.match('a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',emailText))
-            if ('@' in emailText and match==True):
-                print(emailText)
+            match = bool(re.match('\w+@\w+\.{1}\w+',emailText))
+            if ('@' in emailText and match==True) :
                 emailText = emailText.replace(" ",'').replace('\r','')
                 emailText = emailText.replace('\n','').replace('\t','')
                 if (len(mails) == 0) or (emailText not in mails):
-                    print(emailText)
                     mails.append(emailText)
 
 #loops through links on page in order to find more links and pass to the finding mails function.
